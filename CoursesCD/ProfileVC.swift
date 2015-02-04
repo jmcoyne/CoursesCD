0567b51836b37a2c260d58f8b3a334f798aa6004
jmcoyne <jcoyne@mzinga.com>
2015-02-04 16:24:43 -0500

    profile connected

--- CoursesCD/ProfileVC.swift
+++ CoursesCD/ProfileVC.swift
@@ -10,11 +10,34 @@ import UIKit
 
 @objc(ProfileVC) class ProfileVC: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var email: UITextField!
+   
+   
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if let id = defaults.stringForKey("id")
+        {
+            println(id)
+            firstName.text = defaults.stringForKey("firstName")
+            lastName.text = defaults.stringForKey("lastName")
+            email.text = defaults.stringForKey("email")
+            
+            //TODO add API call to get all user data
+            
+            //city.text = defaults.stringForKey("city")
+            // state.text = defaults.stringForKey("state")
+        }
+    
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
