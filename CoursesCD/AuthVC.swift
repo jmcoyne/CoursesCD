19da83fbb016a034c5ff95cb4ad5253e5deb3d75
jmcoyne <jcoyne@mzinga.com>
2015-02-12 08:49:39 -0500

    still a mess, but moving bak to the old version for a bit

--- CoursesCD/AuthVC.swift
+++ CoursesCD/AuthVC.swift
@@ -55,7 +55,7 @@ import CoreData
         
         // Do any additional setup after loading the view.
     }
-    func httpPostWithCustomDelegate() {
+    func httpPostWithCustomDelegate() { 
         // retries when connection has been terminated, avoids request if battery islowor wifi performance is not good
         defaultConfigObject.discretionary = true
         
@@ -88,35 +88,29 @@ import CoreData
                 let httpResponse = response as NSHTTPURLResponse
                 NSLog("Received HTTP \(httpResponse.statusCode)")
                 if httpResponse.statusCode == 200 {
-                    let body = NSString(data: data, encoding: NSUTF8StringEncoding)
-                    NSLog("Response from \(req.URL): \(body)")
-                    self.authCallResults = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: &error) as? NSDictionary
-                    NSLog("Auth Call results = %@", self.authCallResults!);
-                    let successMessage = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_SUCCESS)) as? String
-                    NSLog("Auth Call success = %d", successMessage!)
-                    if successMessage == "z3NgdrZjqXngwrumdYi9" {
-                        NSLog("We got in!")
-                        
-                        self.userDidAuthenticate   = true
-                        let userEmail = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_EMAIL)) as? String
-                        NSLog("My email is %@", userEmail!)
-                        let userFirstName = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_FIRST_NAME)) as? String
-                        let userLastName = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_LAST_NAME)) as? String
-                        let userId = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_ID)) as? NSNumber
-                        let userAvatarURL = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_AVATAR_URL)) as? String
-                        let userAuthToken = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_AUTH_TOKEN)) as? String
-                        NSLog("My authToken is %@", userAuthToken!)
-                        self.defaults.setObject(userEmail, forKey: "email")
-                        self.defaults.setObject(userAuthToken, forKey: "authToken")
-                        self.defaults.setObject(userEmail, forKey: "email")
-                        self.defaults.setObject(userFirstName, forKey: "firstName")
-                        self.defaults.setObject(userLastName, forKey: "lastName")
-                        self.defaults.setObject(userAvatarURL, forKey: "avatarURL")
-                        self.defaults.setObject(userId, forKey: "id")
-                        
-                        
-                        //self.defaults.synchronize()
-                         self.setUserProperties()
+                    ParseUser(userJSON: data)
+                    }
+                   
+                                           self.userDidAuthenticate   = true
+//                        let userEmail = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_EMAIL)) as? String
+//                        NSLog("My email is %@", userEmail!)
+//                        let userFirstName = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_FIRST_NAME)) as? String
+//                        let userLastName = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_LAST_NAME)) as? String
+//                        let userId = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_ID)) as? NSNumber
+//                        let userAvatarURL = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_AVATAR_URL)) as? String
+//                        let userAuthToken = (self.authCallResults?.valueForKeyPath(AUTH_RESULTS_AUTH_TOKEN)) as? String
+//                        NSLog("My authToken is %@", userAuthToken!)
+//                        self.defaults.setObject(userEmail, forKey: "email")
+//                        self.defaults.setObject(userAuthToken, forKey: "authToken")
+//                        self.defaults.setObject(userEmail, forKey: "email")
+//                        self.defaults.setObject(userFirstName, forKey: "firstName")
+//                        self.defaults.setObject(userLastName, forKey: "lastName")
+//                        self.defaults.setObject(userAvatarURL, forKey: "avatarURL")
+//                        self.defaults.setObject(userId, forKey: "id")
+//                        
+//                        
+//                        //self.defaults.synchronize()
+//                         self.setUserProperties()
                         
                     }
                     else {
@@ -134,40 +128,7 @@ import CoreData
         
         task.resume()
     }
-    func setUserProperties() {
-        // 
-        let userEntity = NSEntityDescription.entityForName("User", inManagedObjectContext: self.managedObjectContext!)
-        let user = User(entity: userEntity!, insertIntoManagedObjectContext:self.managedObjectContext!)
-        // Check to see if user is in D
-        let userId:Int = self.defaults.stringForKey("id")!.toInt()!
-        let userFetch = NSFetchRequest(entityName: "User")
-      
-        userFetch.predicate = NSPredicate(format: "id == %d", userId)
-        var fetchError: NSError?
-        // let result = self.managedObjectContext.executeFetchRequest(userFetch, error: &fetchError ) as [User]?
-        let result = self.managedObjectContext?.executeFetchRequest(userFetch, error: &fetchError)  as [User]?
-        if let users = result {
-            if users.count == 0 {
-                println("No user here!")
-                currentUser = User(entity: userEntity!,
-                   insertIntoManagedObjectContext: self.managedObjectContext)
-                currentUser?.firstName = self.defaults.stringForKey("firstName")!
-                 currentUser?.lastName = self.defaults.stringForKey("lastName")!
-                currentUser?.id = self.defaults.stringForKey("id")!.toInt()!
-              if self.managedObjectContext?.save(&fetchError) != nil {
-                   println("Could not save: ")
-                }
-
-            }
-            else{
-                println("All set")
-                
-            }
-        }
-        
-      
-    }
-    override func didReceiveMemoryWarning() {
+       override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
